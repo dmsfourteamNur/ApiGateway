550108b40c7f86967a1c412512b89117eabbf867
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-09-16 00:37:01 -0400

    🚀
    🚀

---
+++ FourteamApiGateway/FourteamApiGateway/Aggregator/UsersPostAgregator.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using ejemplo.Dto;
+using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+
+
+namespace ejemplo.Aggregator
+{
+    public class UsersPostAgregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            if (responses.Any(x => x.Items.Errors().Count > 0))
+            {
+                return new DownstreamResponse(null, HttpStatusCode.BadRequest, null as List<Header>, null);
+            }
+
+            var userResponseContent = await responses[0].Items.DownstreamResponse().Content.ReadAsStringAsync();
+            var postResponseContent = await responses[1].Items.DownstreamResponse().Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<List<User>?>(userResponseContent);
+            var posts = JsonConvert.DeserializeObject<List<Post>?>(postResponseContent);
+
+
+
+            foreach (var user in users!)
+            {
+                var userPosts = posts?.Where(p => p.UserId == user.Id).ToList();
+                user.Posts?.AddRange(userPosts!);
+            }
+
+            var postByUserString = JsonConvert.SerializeObject(users);
+            var stringContent = new StringContent(postByUserString)
+
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+
+        }
+    }
+}
---
+++ FourteamApiGateway/FourteamApiGateway/Startup.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ejemplo.Aggregator;
+using ejemplo.Handlers;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace ejemplo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       ValidateIssuer = false,
+                       ValidateAudience = false,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("alvaro_siles_estrada_llave_secret")),
+                       ClockSkew = new System.TimeSpan(0)
+                   };
+               });
+
+            services.AddOcelot()
+               .AddDelegatingHandler<RemoveEncodingDelegatingHandler>(true)
+               .AddSingletonDefinedAggregator<UsersPostAgregator>();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseOcelot().Wait();
+        }
+    }
+}
